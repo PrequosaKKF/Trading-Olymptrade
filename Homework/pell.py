ae9ef91ce75d5206e6a195d064813b9999ed09c5
Kanisorn Khuphiran <Kanisorn4682@hotmail.com>
2018-07-16 17:07:53 +0700

    changes

--- Homework/pell.py
+++
@@ -1,7 +0,0 @@
-def pell_oblong(ran):
-    for x in range(ran):
-        for y in range(ran):
-            if (x**2)-(2*(y**2)) == -1:
-                n = (x-1)/2
-                U = n*(n+1)/2
-                print("(x,y) = " + str((x,y)) +"and U = " +str(U))
--- resources/JS/script.js
+++
@@ -1,5 +0,0 @@
-var $elem = document.getElementById('output');    
-$elem.innerHTML += 'Test';
-$elem.innerHTML += 4+6;
-$elem.innerHTML += 4+6+'Demo';
-$elem.innerHTML += 'Demo' +4+6;
